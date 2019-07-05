9be40a5d5e4089431b4c39c7fddfad6afdefad6f
moncete <moncete@gmail.com>
2019-07-05 13:56:32 +0200

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,90 @@
+pipeline {
+
+    agent any
+
+    stages {
+        stage ("Clone ansible_devops") {
+            steps {
+                checkout(
+                    [$class: 'GitSCM', 
+                    branches: [[name: '*/master']], 
+                    doGenerateSubmoduleConfigurations: false, 
+                    extensions: [
+                        [$class: 'RelativeTargetDirectory', 
+                         relativeTargetDir: 'ansible-devops'], 
+                        [$class: 'CleanBeforeCheckout']], 
+                        submoduleCfg: [], 
+                        userRemoteConfigs: [
+                            [credentialsId: 'bitbu', url: 'https://aqdes-stash.cm.es/stash/scm/arqdvo/ansible-devops.git']
+                        ]
+                    ]
+                )
+            }
+        }
+
+        stage ("Clone Filebeat") {
+            steps {
+                checkout(
+                    [$class: 'GitSCM', 
+                    branches: [[name: '*/master']], 
+                    doGenerateSubmoduleConfigurations: false, 
+                    extensions: [
+                        [$class: 'RelativeTargetDirectory', 
+                         relativeTargetDir: 'filebeat'], 
+                        [$class: 'CleanBeforeCheckout']], 
+                        submoduleCfg: [], 
+                        userRemoteConfigs: [
+                            [credentialsId: 'bitbu', url: 'https://aqdes-stash.cm.es/stash/scm/arqdvo/trazas-aws-devops.git']
+                        ]
+                    ]
+                )
+            }
+        }
+        
+        stage ("Intalacion Telegraf") {
+            steps {
+                
+                sh '''
+                    cd ansible_devops
+                '''
+
+                ansiblePlaybook (
+                    become: true, 
+                    becomeUser: 'SV00967', 
+                    colorized: true, 
+                    credentialsId: '343c97f1-a663-4d78-94ca-5ed66a5fb039', 
+                    disableHostKeyChecking: true, 
+                    extras: 'IMAGE_VERSION=1.10 ENTORNO=EPD SERVICE=telegraf action=start', 
+                    inventory: "inventory/${params.ENTORNO}", 
+                    playbook: 'pb-devops-infra.yml'
+                )
+
+                sh '''
+                    cd ansible-devops
+                    cat ansible.cfg
+                    ls -l ansible.cfg
+                ''' 
+            }
+        }
+
+        stage ("Instalcion de Filebeat") {
+            steps {
+                sh '''
+                    cd filebeat
+                    cat ansible.cfg
+                    ls -l ansible.cfg
+                '''
+                ansiblePlaybook (
+                    become: true, 
+                    becomeUser: 'SV00967', 
+                    colorized: true, 
+                    credentialsId: '343c97f1-a663-4d78-94ca-5ed66a5fb039', 
+                    disableHostKeyChecking: true, 
+                    extras: 'IMAGE_VERSION=1.10 ENTORNO=EPD SERVICE=telegraf action=start', 
+                    inventory: "inventory/filebeat/container/filebeat-${params.ENTORNO}", 
+                    playbook: 'pb-devops-infra.yml'
+                )
+            }
+        }
+    }
+}
